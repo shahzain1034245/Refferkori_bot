5f89da1eed29c22c03f964511d43d29fe1bc0232
shahzain1034245 <shahzain10212024@gmail.com>
2025-03-28 17:48:45 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-python3 main.py
+
+touch start.sh
+echo "#!/bin/bash" > start.sh
+echo "pip install -r requirements.txt" >> start.sh
+echo "python main.py" >> start.sh
+chmod +x start.sh
