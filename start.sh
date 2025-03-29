1d353126075ce1bea387c90675802b963c70efaf
shahzain1034245 <shahzain10212024@gmail.com>
2025-03-28 17:51:46 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
-
+#!/bin/bash
+apt-get update && apt-get install -y sqlite3 libsqlite3-dev
+pip install -r requirements.txt
+python main.py
 touch start.sh
 echo "#!/bin/bash" > start.sh
 echo "pip install -r requirements.txt" >> start.sh
